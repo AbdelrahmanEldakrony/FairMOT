e77d77c42b8b31959b8b97e446a47acaf48b7370
Abdelrahman10 <eldakrony@kanal-netz.de>
2022-10-04 18:34:49 +0200

    refactoring

--- scripts/loss/losses.py
+++ scripts/loss/losses.py
@@ -33,27 +33,4 @@ def modified_focal_loss(pred, gt):
 
 
 
-# def modified_focal_loss(preds, targets):
-#   pos_inds = targets == 1  # todo targets > 1-epsilon ?
-#   neg_inds = targets < 1  # todo targets < 1-epsilon ?
 
-#   neg_weights = torch.pow(1 - targets[neg_inds], 4)
-
-#   loss = 0
-#   for pred in preds:
-#     pred = torch.clamp(torch.sigmoid(pred), min=1e-4, max=1 - 1e-4)
-#     pos_pred = pred[pos_inds]
-#     neg_pred = pred[neg_inds]
-
-#     pos_loss = torch.log(pos_pred) * torch.pow(1 - pos_pred, 2)
-#     neg_loss = torch.log(1 - neg_pred) * torch.pow(neg_pred, 2) * neg_weights
-
-#     num_pos = pos_inds.float().sum()
-#     pos_loss = pos_loss.sum()
-#     neg_loss = neg_loss.sum()
-
-#     if pos_pred.nelement() == 0:
-#       loss = loss - neg_loss
-#     else:
-#       loss = loss - (pos_loss + neg_loss) / num_pos
-#   return loss
